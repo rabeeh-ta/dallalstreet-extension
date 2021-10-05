a1ffa6c333add5ac2b351fbe22ec0d424aefd1bb
Rabeeh T A <rabeehchemnad@gmail.com>
2021-10-05 15:32:11 +0530

    postgress connected to heroku

--- server/app.js
+++ server/app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
 
+// give the info on how to use the api
 app.get('/', (req, res) => {
   res.send('to get the newest companies .get() /date/dd-mm-yyyy');
 });
-app.get('/date/:date', (req, res) => {
-  res.send('Hello World!');
+
+// query ALL the companies
+app.get('/companies', (req, res) => {
+  db.getAllCompany().then(
+    (data) => res.send(data),
+    (error) => res.send(error)
+  );
+});
+
+// query companies from a given date
+app.get('/companies/from/:date', (req, res) => {
+  res.send('pass the date');
+  db.getAllCompany().then(
+    (resolve) => console.log(resolve),
+    (reject) => console.log(reject)
+  );
   console.log(req.params.date);
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("server started");
+  console.log('server started');
 });
--- server/db.js
+++ server/db.js
@@ -1,22 +1,32 @@
 const { Client } = require('pg');
+require('dotenv');
 
 const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+client.connect();
+/*
   user: 'postgres',
   host: 'localhost',
   database: 'dallalstreet',
   password: 'rabeeh',
   port: 5432,
-});
-client.connect();
+*/
 
-// list everything
-function listAllStocks() {
-  client.query('SELECT * FROM COMPANY;', (err, res) => {
-    client.end();
-    return res.rows;
+// query and get everything in db
+var getAllCompany = function () {
+  return new Promise((resolve, reject) => {
+    client.query('SELECT * FROM users;', (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.rows);
+      }
+    });
   });
-}
+};
 
-listAllStocks();
-
-module.exports = { listAllStocks };
+module.exports = { getAllCompany };
