dbc71469d8744f3792cc98ff334239869532307a
Rabeeh T A <rabeehchemnad@gmail.com>
2021-10-06 13:19:36 +0530

    add companies to db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-require('dotenv');
+require('dotenv').config();
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -14,12 +14,17 @@ client.connect();
   database: 'dallalstreet',
   password: 'rabeeh',
   port: 5432,
+-------------------------------------------------
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 */
 
-// query and get everything in db
+//? query and get everything in db
 var getAllCompany = function () {
   return new Promise((resolve, reject) => {
-    client.query('SELECT * FROM users;', (err, res) => {
+    client.query('SELECT * FROM companies;', (err, res) => {
       if (err) {
         reject(err);
       } else {
@@ -29,4 +34,31 @@ var getAllCompany = function () {
   });
 };
 
-module.exports = { getAllCompany };
+//? query from a specific date
+var getCompany = function (name, symbol, date) {
+  return new Promise((resolve, reject) => {
+    client.query('INSERT INTO ');
+  });
+};
+
+//? add new company to the db
+var addCompany = function (name, symbol) {
+  let date = new Date().toISOString().slice(0, 10);
+
+  return new Promise((resolve, reject) => {
+    client.query(
+      'INSERT INTO companies (name,smbol,added_date) VALUES ($1,$2,$3)',
+      [name, symbol, date],
+      (err, res) => {
+        if (err) {
+          reject('error');
+          console.log(err);
+        } else {
+          resolve(res.rows);
+        }
+      }
+    );
+  });
+};
+
+module.exports = { getAllCompany, getCompany, addCompany };
