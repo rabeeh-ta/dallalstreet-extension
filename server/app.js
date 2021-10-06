ea42c7ce703899a8d04e8048fe10f76dc3f0f764
Rabeeh T A <rabeehchemnad@gmail.com>
2021-10-06 13:20:14 +0530

    post() route working for adding new companies

--- server/app.js
+++ server/app.js
@@ -2,29 +2,39 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // give the info on how to use the api
 app.get('/', (req, res) => {
-  res.send('to get the newest companies .get() /date/dd-mm-yyyy');
+  res.json({
+    '/': 'info route',
+    '/companies': 'list all the companies in db',
+    '/companies/from/yyyy-mm-dd':
+      'get all the companies listed after the given date',
+    '/companies/add': '.post() new company (name,symbol,added_date)',
+  });
 });
 
 // query ALL the companies
 app.get('/companies', (req, res) => {
-  db.getAllCompany().then(
-    (data) => res.send(data),
-    (error) => res.send(error)
-  );
+  db.getAllCompany()
+    .then((succuss) => res.send(succuss))
+    .catch((err) => res.send(err));
 });
 
 // query companies from a given date
 app.get('/companies/from/:date', (req, res) => {
   res.send('pass the date');
-  db.getAllCompany().then(
-    (resolve) => console.log(resolve),
-    (reject) => console.log(reject)
-  );
   console.log(req.params.date);
 });
 
+app.post('/companies/add', (req, res) => {
+  db.addCompany(req.body.name, req.body.symbol)
+    .then((succuss) => res.send('new company added'))
+    .catch((err) => res.send(err));
+});
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log('server started');
+  console.log('server started at localhost:3000');
 });
