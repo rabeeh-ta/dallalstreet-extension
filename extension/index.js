aa2660626950a4236579a8f07f33b303a59a48d2
Rabeeh T A <rabeehchemnad@gmail.com>
2021-10-18 12:17:48 +0530

    CORS error fix

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,16 @@
 const refreshBtn = document.getElementById('refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
-  alert('hello');
+  console.log('click event');
+  fetch('https://www.google.com/search?q=itc+screener')
+    .then((res) => {
+      console.log(res.text());
+    })
+    .then((HtmlWebpage) => {
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(HtmlWebpage, 'text/html');
+      console.log(doc);
+    });
 });
+
+console.log('after function');
