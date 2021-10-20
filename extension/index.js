3a0537650406c1bcd8738930a24269f4e63e2d4b
rabeeh-ta <rabeehchemnad@gmail.com>
2021-10-20 12:34:58 +0530

    query button mvp

--- extension/index.js
+++ extension/index.js
@@ -1,14 +1,26 @@
 var redirectLinks = {};
+var allRedirectSource = [];
 const refreshBtn = document.getElementById('refresh-btn');
+const allRedirectBtns = document.getElementsByClassName('redirect-link');
 
 refreshBtn.addEventListener('click', () => {
   //clear the previous query
   redirectLinks = new Object();
-  console.log('click event');
+  allRedirectSource = [];
+
+  // get all the buttons id to query for
+  // console.log(allRedirectBtns);
+  for (var item of allRedirectBtns) {
+    mrMehta('zomato', item.id);
+  }
+
+  // query google and get the links
+  /*
   mrMehta('hcl tech', 'trendlyne');
   mrMehta('hcl tech', 'tickertape');
   mrMehta('hcl tech', 'screener');
   mrMehta('hcl tech', 'wikipedia');
+  */
 });
 
 // query google and scrape and return the first link from the result
@@ -16,7 +28,6 @@ function mrMehta(compName, sourceName) {
   fetch(`https://www.google.com/search?q=${compName}+${sourceName}`).then(
     (res) => {
       res.text().then((text) => {
-        console.log(typeof text);
         const doc = new DOMParser().parseFromString(text, 'text/html');
         const classElements = doc.getElementsByClassName('yuRUbf');
         const sourceLink = classElements[0].firstChild.getAttribute('href');
