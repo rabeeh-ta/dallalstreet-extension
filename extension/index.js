bbe46140bb45c4d70adb89f22600cd8343c4a83b
rabeeh-ta <rabeehchemnad@gmail.com>
2021-10-20 11:36:53 +0530

    google search scrape working

--- extension/index.js
+++ extension/index.js
@@ -1,16 +1,30 @@
+var redirectLinks = {};
 const refreshBtn = document.getElementById('refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
+  //clear the previous query
+  redirectLinks = new Object();
   console.log('click event');
-  fetch('https://www.google.com/search?q=itc+screener')
-    .then((res) => {
-      console.log(res.text());
-    })
-    .then((HtmlWebpage) => {
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(HtmlWebpage, 'text/html');
-      console.log(doc);
-    });
+  mrMehta('hcl tech', 'trendlyne');
+  mrMehta('hcl tech', 'tickertape');
+  mrMehta('hcl tech', 'screener');
+  mrMehta('hcl tech', 'wikipedia');
 });
+
+// query google and scrape and return the first link from the result
+function mrMehta(compName, sourceName) {
+  fetch(`https://www.google.com/search?q=${compName}+${sourceName}`).then(
+    (res) => {
+      res.text().then((text) => {
+        console.log(typeof text);
+        const doc = new DOMParser().parseFromString(text, 'text/html');
+        const classElements = doc.getElementsByClassName('yuRUbf');
+        const sourceLink = classElements[0].firstChild.getAttribute('href');
+        redirectLinks[sourceName] = sourceLink;
+        console.log(sourceLink);
+      });
+    }
+  );
+}
 
 console.log('after function');
